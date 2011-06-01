c1d16b976e1a3658c3e08ec7b886757e067018bc
Konstantin Koss <konstantin.koss@gmail.com>
2011-06-01 22:54:31 +0200

    Initial interface for following

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,21 +7,41 @@ describe PagesController do
   end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+    
+    describe "when not signed in" do
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'home'
+        response.should have_selector("title", 
+                                      :content => "#{@basetitle} | Home")
+      end
+
+      it "should not have a blank body" do
+        get 'home'
+        response.body.should_not =~ /<body>\s*<\/body>/      
+      end
     end
     
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title", 
-                                    :content => "#{@basetitle} | Home")
+    describe "when signed in" do
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user.follow!(@user)
+      end
+      
+      it "should have the right follower-following counts" do
+        get :home
+        response.should have_selector('a', :href => following_user_path(@user),
+                                           :content => "0 following")
+        response.should have_selector('a', :content => "1 follower", 
+                                           :href => followers_user_path(@user))
+      end
     end
     
-    it "should not have a blank body" do
-      get 'home'
-      response.body.should_not =~ /<body>\s*<\/body>/      
-    end
   end
 
   describe "GET 'contact'" do
